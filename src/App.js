5bd072e243157600b873641b344b99c21951e26f
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-12-20 23:27:39 +0100

    Upload folders

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours);
+  }
+  
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error)
+    }
+  }
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main className="title">
+        <h2>No tours left</h2>
+        <button 
+          className="btn" 
+          onClick = {
+            () => fetchTours()
+            // fetchTours
+          }>
+          Refresh
+        </button>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+
+export default App
